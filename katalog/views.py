94e04ad22768759ac9467e6118d9fcfd29cffd1d
gabiiing13 <gabiiing13@gmail.com>
2022-09-15 06:38:16 +0700

    remove todos text and fix homepage

--- katalog/views.py
+++ katalog/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from katalog.models import CatalogItem
 
-# TODO: Create your views here.
 def show_katalog(request):
     data_catalog = CatalogItem.objects.all()
     context = {
