c47af968ad265cbe501d257a6a609c0e96db087b
gabiiing13 <gabiiing13@gmail.com>
2022-09-14 21:01:27 +0700

    finish setting models-views-templates

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_catalog = CatalogItem.objects.all()
+    context = {
+        'list_barang':data_catalog,
+        'nama': 'Gabriel Zebaoth Krisopras Putra',
+        'student_id' : '2106751480',
+    }
+
+    return render(request, "katalog.html", context)
