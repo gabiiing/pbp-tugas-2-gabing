ca0a74defb862813a1e2b01c664585f81791d47c
gabiiing13 <gabiiing13@gmail.com>
2022-09-15 11:58:57 +0700

    fix test again

--- katalog/tests.py
+++ katalog/tests.py
@@ -8,11 +8,11 @@ class katalog_test(TestCase):
                                    description="New Iphone", rating=5, item_url="https://www.tokopedia.com/spiritcellular-1/iphone-xr-64gb-second-e-x-inter-original-no-minus-fullset-kuning?extParam=ivf%3Dfalse&src=topads")
 
     def test_is_dummy_valid(self):
-        cap = CatalogItem.objects.get(item_name="Iphone XR", item_price=4000000, item_stock=100,
+        iphone_XR = CatalogItem.objects.get(item_name="Iphone XR", item_price=4000000, item_stock=100,
                                       description="New Iphone", rating=5, item_url="https://www.tokopedia.com/spiritcellular-1/iphone-xr-64gb-second-e-x-inter-original-no-minus-fullset-kuning?extParam=ivf%3Dfalse&src=topads")
-        self.assertEqual(cap.item_name, "Iphone XR")
-        self.assertEqual(cap.item_price, 4000000)
-        self.assertEqual(cap.item_stock, 100)
-        self.assertEqual(cap.description, "New Iphone")
-        self.assertEqual(cap.rating, 5)
-        self.assertEqual(cap. item_url, "https://www.tokopedia.com/spiritcellular-1/iphone-xr-64gb-second-e-x-inter-original-no-minus-fullset-kuning?extParam=ivf%3Dfalse&src=topads")
+        self.assertEqual(iphone_XR.item_name, "Iphone XR")
+        self.assertEqual(iphone_XR.item_price, 4000000)
+        self.assertEqual(iphone_XR.item_stock, 100)
+        self.assertEqual(iphone_XR.description, "New Iphone")
+        self.assertEqual(iphone_XR.rating, 5)
+        self.assertEqual(iphone_XR. item_url, "https://www.tokopedia.com/spiritcellular-1/iphone-xr-64gb-second-e-x-inter-original-no-minus-fullset-kuning?extParam=ivf%3Dfalse&src=topads")
