a585fd2d8bc40418ee1a8f56e775e3ae1aa44c19
gabiiing13 <gabiiing13@gmail.com>
2022-09-15 13:14:58 +0700

    Mengerjakan Bonus yang masih  --- salah menggunakan method

--- katalog/tests.py
+++ katalog/tests.py
@@ -2,9 +2,9 @@ from django.test import TestCase
 from katalog.models import CatalogItem
 
 # Create your tests here.
-class katalog_test(TestCase):
+class katalogTest(TestCase):
     def setUp(self):
-        CatalogItem.objects.get(item_name="Iphone XR", item_price=4000000, item_stock=100,
+        CatalogItem.objects.create(item_name="Iphone XR", item_price=4000000, item_stock=100,
                                    description="New Iphone", rating=5, item_url="https://www.tokopedia.com/spiritcellular-1/iphone-xr-64gb-second-e-x-inter-original-no-minus-fullset-kuning?extParam=ivf%3Dfalse&src=topads")
 
     def test_is_dummy_valid(self):
