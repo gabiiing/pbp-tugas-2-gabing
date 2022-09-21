f300cb7db1d9f1e7790c31d80988caedeeb9756f
gabiiing13 <gabiiing13@gmail.com>
2022-09-21 21:40:31 +0700

    add simple unittest

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from mywatchlist.views import show_my_watch_list, show_xml, show_json
 
-# Create your tests here.
+
+class Test_Data_Delivery_URLs(SimpleTestCase):
+    def test_data_delivery_HTML(self):
+        url = reverse('mywatchlist:show_my_watch_list')
+        print(resolve(url))
+        self.assertEqual(resolve(url).func, show_my_watch_list)
+    def test_data_delivery_JSON(self):
+        url = reverse('mywatchlist:show_json')
+        print(resolve(url))
+        self.assertEqual(resolve(url).func, show_json)
+    def test_data_delivery_XML(self):
+        url = reverse('mywatchlist:show_xml')
+        print(resolve(url))
+        self.assertEqual(resolve(url).func, show_xml)
+    
+
