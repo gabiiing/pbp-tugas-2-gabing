d29736fd3ccfd51fcaf56ece01c523a76a0904cd
gabiiing13 <gabiiing13@gmail.com>
2022-09-22 11:35:41 +0700

    fix test.py

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,13 +1,13 @@
 from django.test import SimpleTestCase, TestCase
 from django.urls import reverse, resolve
-from mywatchlist.views import show_my_watch_list, show_xml, show_json
+from mywatchlist.views import show_html, show_xml, show_json
 
 
 class Test_Data_Delivery_URLs(TestCase):
     def test_data_delivery_HTML(self):
-        url = reverse('mywatchlist:show_my_watch_list')
+        url = reverse('mywatchlist:show_html')
         print(resolve(url))
-        self.assertEqual(resolve(url).func, show_my_watch_list)
+        self.assertEqual(resolve(url).func, show_html)
     def test_data_delivery_JSON(self):
         url = reverse('mywatchlist:show_json')
         print(resolve(url))
